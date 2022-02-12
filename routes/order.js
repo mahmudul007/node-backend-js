2c13afe329322a30bc0211a2312e4a8e1d512088
mahmudul007 <62248559+mahmudul007@users.noreply.github.com>
2022-02-12 23:06:35 +0600

    order last update

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { aggregate } = require("../Model/Order");
 const Order = require('../Model/Order');
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAdmin } = require("./verifyToken");
 
@@ -68,7 +69,48 @@ router.get("/", verifyTokenAdmin, async (req, res) => {
         res.status(500).json(err);
     }
 
-})
+});
+
+//get monthly income
+router.get("/income", verifyTokenAdmin, async (req, res) => {
+
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try {
+        const income = await Order.aggregate([
+            { $match: { createdAt: { gte: previousMonth } } },
+
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "amount"
+
+
+                }
+            }
+
+
+            , {
+                $group: {
+                    _id: "month",
+                    total: { $sum: "sales" }
+                }
+            }
+        ]
+        )
+        res.status(200).json(income);
+
+
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+
+
+});
+
 
 
 
